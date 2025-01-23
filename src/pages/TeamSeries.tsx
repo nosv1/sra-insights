2812bd9da65cab2e8483c98a8665d09272dd56c3
Chris Seagraves <47610393+nosv1@users.noreply.github.com>
2025-01-23 01:37:24 -0600

    ... pre release of team series like

---
+++ src/pages/TeamSeries.tsx
@@ -0,0 +1,16 @@
+
+import ArcadeLeaderboard from '../components/ArcadeLeaderboard';
+
+const TeamSeries: React.FC = () => {
+    return (
+        <div>
+            <h1>Team Series</h1>
+            <ArcadeLeaderboard />
+            <ArcadeLeaderboard />
+            <ArcadeLeaderboard />
+            <ArcadeLeaderboard />
+            <ArcadeLeaderboard />
+        </div>
+    );
+}
+export default TeamSeries;
