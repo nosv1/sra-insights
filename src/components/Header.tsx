32587e686a22e81bc47247abc1e4e72969394e32
Chris Seagraves <47610393+nosv1@users.noreply.github.com>
2025-02-11 18:33:06 -0600

    fix: update tooltip to display best valid lap time in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -38,7 +38,7 @@ export const Header: React.FC = () => {
                             <div className="tooltip">
                                 {session.carDrivers?.map(carDriver => (
                                     <div key={carDriver.driverId}>
-                                        D{carDriver.basicDriver?.raceDivision} | {carDriver.basicDriver?.name}: {carDriver.sessionCar?.bestLap?.lapTime && Lap.timeToString(carDriver.sessionCar?.bestLap?.lapTime)} ({carDriver.sessionCar?.laps.length} laps)
+                                        D{carDriver.basicDriver?.raceDivision} | {carDriver.basicDriver?.name}: {carDriver.sessionCar?.bestValidLap?.lapTime && Lap.timeToString(carDriver.sessionCar?.bestValidLap?.lapTime)} ({carDriver.sessionCar?.laps.length} laps)
                                     </div>
                                 ))}
                             </div>
