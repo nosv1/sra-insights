cbf607bc5d75d2a7eed3e816689343757d7a6fe7
Chris Seagraves <47610393+nosv1@users.noreply.github.com>
2025-03-29 16:34:07 -0500

    feat: enhance LeaderboardsPage to support series selection and update TeamSeriesLeaderboards component for dynamic server and track name handling

--- src/pages/LapTimes/LeaderboardsPage.tsx
+++ src/pages/LapTimes/LeaderboardsPage.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { Footer } from '../../components/Footer';
 import { Header } from '../../components/Header';
 import { TeamSeriesLeaderboards } from '../../components/Leaderboards/TeamSeriesLeaderboards';
 
+
 export const LeaderboardsPage: React.FC = () => {
+
+    const location = useLocation();
+
+    const getParams = () => {
+        const params = new URLSearchParams(location.search);
+        const seriesParam = params.get('series');
+        const series = seriesParam === 'endurance-series' ? 'endurance-series' : 'team-series';
+        return {
+            series: series,
+        };
+    };
+
+    let params = getParams();
+
+    const [seriesState, setSeries] = useState<'team-series' | 'endurance-series'>(params.series as 'team-series' | 'endurance-series');
+
     return (
         <div>
             <Header />
-            <TeamSeriesLeaderboards />
+            <TeamSeriesLeaderboards series={seriesState} />
             <Footer />
         </div>
     )
